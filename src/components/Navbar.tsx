1bb1cea6599fcaa27aee356d40ee36df4f45c65f
seojoonrp <seojoonrp@snu.ac.kr>
2026-02-15 23:54:55 +0900

    feat: Add navbar design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,33 @@
 import Link from "next/link";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/team">Team</Link>
-      <Link href="/publications">Publications</Link>
-      <Link href="/join-us">Join Us</Link>
-    </nav>
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        snumpr lab
+      </Link>
+
+      <nav className={styles.nav}>
+        <Link href="/team">Team</Link>
+        <Link href="/publications">Publications</Link>
+        <Link href="/join-us">Join Us</Link>
+
+        <a
+          href="https://github.com/snumprlab"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <a
+          href="https://huggingface.co/SNUMPR"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          HuggingFace
+        </a>
+      </nav>
+    </header>
   );
 }
